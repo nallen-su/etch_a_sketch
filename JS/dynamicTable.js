9c77aec9a1ecea17b4c0cda9c133ecb0f4c75b76
Nicholas Allen <nick.k.allen92@gmail.com>
2016-12-05 16:37:37 -0500

    Upload with functioning initial loading, black fill button, color fill button, and clear button

--- JS/dynamicTable.js
+++ JS/dynamicTable.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
-	newGrid(16);
 
+	newGrid(50);
+
+	$(".box").mouseenter(function() {
+		$(this).css("background-color","black");
+	});
+
+	$("#colorGrid").click(function() {
+		$(".box").mouseenter(function() {
+			$(this).css("background-color",getRandomColor());
+		});
+	});
+
+	$("#blackGrid").click(function() {
+		$(".box").mouseenter(function() {
+			$(this).css("background-color","black");
+		});
+	});
+	
 });
-
-
-
-
 
 function newGrid(dimension) {
 	var $sketch = $("#sketch");
@@ -21,3 +34,36 @@ function newGrid(dimension) {
 	var size = 800 / dimension;
 	$(".box").css({"height":size+"px", "width":size+"px"});
 } 
+
+function clearGrid() {
+	$(".box").data("grayscale",0);
+	$(".box").css({"background-color":"white"});
+}
+
+function userInputGrid() {
+	var number = newGrid(prompt("How many rows and columns would you like?"));
+	return number;
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.round(Math.random() * 15)];
+    }
+    return color;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
